3bc8f64b48d9e92cefbd59d7c38cccfd3e18a898
minhnhatdang2022-byte <minhnhatdang2022@gmail.com>
2026-01-02 21:22:22 +0700

    Update dam.js

--- js/dam.js
+++ js/dam.js
@@ -1,74 +1,38 @@
 import { db } from "./firebase.js";
-import { ref, push, onValue, remove, update } 
+import { ref, push, onValue, update, remove } 
 from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
 
 const id = new URLSearchParams(location.search).get("id");
-const damRef = ref(db, "dams/" + id);
-
-let gifts = {}, costs = {};
-
-window.back = () => history.back();
-
-onValue(damRef, snap => {
-  const d = snap.val();
-  title.innerText = d.name;
-  gifts = d.gifts || {};
-  costs = d.costs || {};
-  render();
-});
+const giftRef = ref(db, `dams/${id}/gifts`);
 
 window.addGift = () => {
   if (!name.value || !money.value) return;
-  push(ref(db, `dams/${id}/gifts`), {
+  push(giftRef, {
     name: name.value,
     money: Number(money.value)
   });
   name.value = money.value = "";
 };
 
-window.addCost = () => {
-  if (!costName.value || !costMoney.value) return;
-  push(ref(db, `dams/${id}/costs`), {
-    name: costName.value,
-    money: Number(costMoney.value)
-  });
-  costName.value = costMoney.value = "";
-};
-
-window.del = path => {
-  if (confirm("Xóa?")) remove(ref(db, path));
-};
-
-window.edit = (path, field, value) => {
-  update(ref(db, path), { [field]: value });
-};
-
-window.render = () => {
-  const k = searchName.value.toLowerCase();
-  let tg = 0, tc = 0;
-
+onValue(giftRef, snap => {
+  let sum = 0;
   giftList.innerHTML = "";
-  Object.entries(gifts).forEach(([i, g]) => {
-    if (!g.name.toLowerCase().includes(k)) return;
-    tg += g.money;
+  snap.forEach(c => {
+    const d = c.val();
+    sum += d.money;
     giftList.innerHTML += `
-      <div class="item">
-        ${g.name} - ${g.money}
-        <button onclick="del('dams/${id}/gifts/${i}')">X</button>
-      </div>`;
+      <tr>
+        <td>${d.name}</td>
+        <td>${d.money.toLocaleString()}</td>
+        <td>
+          <button onclick="delGift('${c.key}')">❌</button>
+        </td>
+      </tr>`;
   });
+  total.innerText = "Tổng tiền: " + sum.toLocaleString() + " đ";
+});
 
-  costList.innerHTML = "";
-  Object.entries(costs).forEach(([i, c]) => {
-    tc += c.money;
-    costList.innerHTML += `
-      <div class="item">
-        ${c.name} - ${c.money}
-        <button onclick="del('dams/${id}/costs/${i}')">X</button>
-      </div>`;
-  });
-
-  totalGift.innerText = tg;
-  totalCost.innerText = tc;
-  profit.innerText = tg - tc;
+window.delGift = key => {
+  if (confirm("Xoá người này?"))
+    remove(ref(db, `dams/${id}/gifts/${key}`));
 };
