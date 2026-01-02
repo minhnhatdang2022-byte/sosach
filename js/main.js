8051a06c6541036c8f7e1fd1ac0cc17dbb1923fc
minhnhatdang2022-byte <minhnhatdang2022@gmail.com>
2026-01-02 21:14:37 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,43 @@
 import { db } from "./firebase.js";
-import { ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { ref, push, onValue, remove } 
+from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
 
 const damRef = ref(db, "dams");
+let cache = {};
 
 window.addDam = () => {
-  const name = damName.value;
-  const date = damDate.value;
+  const name = damName.value.trim();
   if (!name) return alert("Nhập tên đám");
+  push(damRef, { name });
+  damName.value = "";
+};
 
-  push(damRef, { name, date });
+window.render = () => {
+  const keyword = searchDam.value.toLowerCase();
+  list.innerHTML = "";
+
+  Object.entries(cache).forEach(([id, dam]) => {
+    if (!dam.name.toLowerCase().includes(keyword)) return;
+
+    list.innerHTML += `
+      <div class="item">
+        <b>${dam.name}</b><br>
+        <button onclick="openDam('${id}')">Mở</button>
+        <button onclick="deleteDam('${id}')">Xóa</button>
+      </div>`;
+  });
+};
+
+window.openDam = id => {
+  location.href = `dam.html?id=${id}`;
+};
+
+window.deleteDam = id => {
+  if (confirm("Xóa đám này?"))
+    remove(ref(db, "dams/" + id));
 };
 
 onValue(damRef, snap => {
-  damList.innerHTML = "";
-  const keyword = searchDam.value?.toLowerCase() || "";
-
-  snap.forEach(d => {
-    if (!d.val().name.toLowerCase().includes(keyword)) return;
-
-    damList.innerHTML += `
-      <div class="item">
-        <a href="dam.html?id=${d.key}">${d.val().name}</a>
-      </div>`;
-  });
+  cache = snap.val() || {};
+  render();
 });
-
-searchDam.oninput = () => onValue(damRef, () => {});
