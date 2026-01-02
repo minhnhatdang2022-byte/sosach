88e1382395cd9dd868af5dd105344562329aeb37
minhnhatdang2022-byte <minhnhatdang2022@gmail.com>
2026-01-02 21:13:25 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
+  apiKey: "AIzaSyB0JZrGgVRUp8h3LuFQdwjuFxi_p0p3Zvo",
+  authDomain: "csdl-90950.firebaseapp.com",
+  databaseURL: "https://csdl-90950-default-rtdb.firebaseio.com",
+  projectId: "csdl-90950",
+  storageBucket: "csdl-90950.firebasestorage.app",
+  messagingSenderId: "756108541766",
+  appId: "1:756108541766:web:ab326c8d55ced5e1c22f47"
 };
 
 const app = initializeApp(firebaseConfig);
