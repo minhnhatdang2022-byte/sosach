338ae660b6be0c090df7023026385239913b82c6
minhnhatdang2022-byte <minhnhatdang2022@gmail.com>
2026-01-02 21:38:40 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,8 @@
+// js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
 
+// Cấu hình Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyB0JZrGgVRUp8h3LuFQdwjuFxi_p0p3Zvo",
   authDomain: "csdl-90950.firebaseapp.com",
@@ -8,8 +10,12 @@ const firebaseConfig = {
   projectId: "csdl-90950",
   storageBucket: "csdl-90950.firebasestorage.app",
   messagingSenderId: "756108541766",
-  appId: "1:756108541766:web:ab326c8d55ced5e1c22f47"
+  appId: "1:756108541766:web:ab326c8d55ced5e1c22f47",
+  measurementId: "G-6BR2F8630M"
 };
 
+// Khởi tạo Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+
+// Export database để dùng ở các file khác
+export const database = getDatabase(app);
